372a0fcbbff0c14732559d8d5fa3f493568f9fdc
mixmix <whimful@gmail.com>
2019-05-30 23:10:12 +1200

    all the scripts improved!

--- index.js
+++ index.js
@@ -10,25 +10,47 @@ const versions = [
   { version: 'xyani', format: 'gif', nameTransform: simpleCase }
 ]
 
-pokemon.forEach(name => console.log(simpleCase(name)))
-
-versions.forEach(({ version, format, nameTransform }) => {
-  console.log(`# ${version}\n`)
-  pokemon.forEach(name => {
-    var fileName = `${nameTransform(name)}.${format}`
-    var path = join(__dirname, `sprites/${version}/${fileName}`)
-
-    request(`${baseURL}/${version}/${fileName}`)
-      .on('error', console.log)
-      .pipe(fs.createWriteStream(path))
+versions.forEach((config) => {
+  pokemon.forEach((name, i) => {
+    if (typeof name === 'string') download({ name, number: i + 1, config })
+    else name.forEach(n => download({ name: n, number: i + 1, config }))
+    // nidoran has a male and female version but the same number...
   })
 })
 
+function download ({ name, number, config }) {
+  const { version, format, nameTransform } = config
+
+  var path = join(
+    __dirname,
+    `sprites/${version}/${simpleFilename(name, number)}.${format}`
+  )
+
+  request(`${baseURL}/${version}/${nameTransform(name)}.${format}`)
+    .on('error', console.log)
+    .pipe(fs.createWriteStream(path))
+}
+
 function simpleCase (name) {
+  // this is the format that our baseUrl happens to use.
   return name
     .toLowerCase()
     .replace(' ', '')
     .replace("'", '')
     .replace(/female$/, 'f')
     .replace(/male$/, 'm')
+}
+
+function simpleFilename (name, number) {
+  var _number = Array.from(String(number))
+  while (_number.length < 3) _number.unshift(0)
+
+  _number = _number.join('')
+
+  const _name = name
+    .toLowerCase()
+    .replace(' ', '-')
+    .replace("'", '-')
+
+  return `${_number}.${_name}`
 }
